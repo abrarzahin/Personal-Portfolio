be6cd60434ae3ebbd65f035c1e060e4eceb656b2
abrar zahin <abrarzahin@yahoo.com>
2019-05-18 16:43:05 -0400

    pw

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio',
         'USER':'postgres',
-        'PASSWORD':'1234asdF',
+        'PASSWORD':'*******',
         'HOST':'localhost',
         'PORT':'5432',
     }
